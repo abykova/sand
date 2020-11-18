de0dbf897a6db3ecf3b5b99f767c9af6a2974e74
abykova <alex.94.girl@gmail.com>
2020-11-18 12:49:15 +0600

    correction

--- views/site/graphic.php
+++ views/site/graphic.php
@@ -5,37 +5,36 @@ $this->title = 'График';
 
 <div class="site-index">
 
-	<div class="body-content">
+    <div class="body-content">
 
-		<table class="table">
-			<thead>
-			<tr>
-				<th scope="col">#</th>
-				<th scope="col">Номер платежа</th>
-				<th scope="col">Дата платежа</th>
-				<th scope="col">Платёж в месяц</th>
-				<th scope="col">Сумма погашаемых процентов</th>
-				<th scope="col">Сумма погашаемого основного долга</th>
-				<th scope="col">Остаток основного долга</th>
-			</tr>
-			</thead>
-
-            <?php for ($i = 1; $i <= $mounth; $i++)
-			        { ?>
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Номер платежа</th>
+                <th scope="col">Дата платежа</th>
+                <th scope="col">Платёж в месяц</th>
+                <th scope="col">Сумма погашаемых процентов</th>
+                <th scope="col">Сумма погашаемого основного долга</th>
+                <th scope="col">Остаток основного долга</th>
+            </tr>
+            </thead>
+			<?php for ($i = 1;
+			$i <= $mounth;
+			$i++)
+			{ ?>
             <tbody>
             <tr>
-				<th scope="row"><?php echo $i; ?></th>
-				<td><?php echo $i; ?></td>
-				<td><?php echo $date; ?></td>
-				<td><?php echo round($amountInMonth, 2); ?></td>
-
-				<td><?php echo round($amountOfInterestPaid*$i, 2); ?></td>
-
-				<td><?php echo round($amountInMonth*$i, 2); ?></td>
-				<td><?php echo round($allSum-($amountInMonth*$i), 2);
-			        }  ?></td>
-			</tr>
-			</tbody>
-		</table>
-	</div>
+                <th scope="row"><?php echo $i; ?></th>
+                <td><?php echo $i; ?></td>
+                <td><?php echo $date; ?></td>
+                <td><?php echo round($amountInMonth, 2); ?></td>
+                <td><?php echo round($amountOfInterestPaid * $i, 2); ?></td>
+                <td><?php echo round($amountInMonth * $i, 2); ?></td>
+                <td><?php echo round($allSum - ($amountInMonth * $i), 2);
+					} ?></td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
